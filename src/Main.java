130ab11436ab1da58fd0b4a2a61f3f91b8e891c5
nikolas.caracho <nikolasarruda762@gmail.com>
2025-07-15 08:49:05 -0300

    Arithmetic operators

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Nikolas Caracho";
+        System.out.println("Hello, " + "!" + name);
+
+//basic arithmetic operators
+        int a = 3;
+        int b = 2;
+        int sum = a + b;
+        int subtraction =  a - b;
+        int multiplication = a * b;
+        //floating an int
+        float division = (float)a/b;
+
+        System.out.println("Variable 01 = " + a);
+        System.out.println("Variable 02 = " + b + "\n");
+        System.out.println("Sum = " + sum);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
+    }
+}
